5c33aa29823b2bbdd4c2f2405c54714c0328f409
Vishnu Kumar <vishnukumart96@gmail.com>
2024-07-03 20:24:54 +0530

    add dist folder

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const PORT = 3000;
+const handler = (req, res) => {
+    return res.json({ message: "Hey the F#% your api is working !!" });
+};
+app.listen(PORT, () => {
+    console.log("App is listening on the PORT : 3000");
+});
