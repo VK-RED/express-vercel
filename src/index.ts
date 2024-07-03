981d9a7afcf8cd3a770f50db314928620eb4af14
Vishnu Kumar <vishnukumart96@gmail.com>
2024-07-03 19:59:07 +0530

    Init

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express,{Request,Response} from "express";
+
+const app = express();
+const PORT = 3000;
+
+const handler = (req:Request,res:Response) => {
+
+    return res.json({message:"Hey the F#% your api is working !!"});
+
+}
+
+
+app.listen(PORT,()=>{
+    console.log("App is listening on the PORT : 3000");
+})
